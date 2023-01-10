29342fc03ac0efd614267718f1f5fe0d286e9d72
rbeucher <romain.beucher@anu.edu.au>
2023-01-10 16:17:45 +1100

    Change sendgrid configuration

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -123,8 +123,8 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   config.action_mailer.smtp_settings = {
-    user_name: 'apikey',
-    password: ENV["SENDGRID_API"],
+    user_name: ENV("SENDGRID_USERNAME"),
+    password: ENV["SENDGRID_PASSWORD"],
     address: 'smtp.sendgrid.net',
     domain: 'briefideas.org',
     port: 587,
